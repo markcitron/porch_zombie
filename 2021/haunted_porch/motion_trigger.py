2b1bd0a7e8458156c9310406bbb6a9cb3c823346
Mark Citron <mark_citron@hotmail.com>
2021-10-07 14:07:35 -0700

    wip

--- 2021/haunted_porch/motion_trigger.py
+++ 2021/haunted_porch/motion_trigger.py
@@ -22,10 +22,12 @@ def main():
     for line in tailer.follow(open(motion_log)):
         if start_trigger in line:
             print("Motion detected.")
+            # Wake up Zombie1
             call_status, call_message = wrapped_wget("http://10.10.0.3:5000/wake/")
             time.sleep(30)
         if end_trigger in line:
             print("No more motion.")
+            # Go to sleep Zombie1
             call_status, call_message = wrapped_wget("http://10.10.0.3:5000/sleep/")
             time.sleep(30)
         if not call_status:
---
+++ 2021/zombie3_stepper/Readme.md
@@ -0,0 +1,6 @@
+# Zombie3 - stepper controlled ghost
+Will have elevated track and a stepper motor will be triggered to move a our zombie back and forth.  The zombie will be suspended by a strint/wire attacked to the payload cart the the stepper will move.
+## 3rd party stuff ...
+- [FRV8825.py stepper library](../stepper_test/DRV8825.py) - third party library I got from the folks where I got my stepper hat controller.
+## Required Includes
+-- Flask - `pip3 install Flask`
