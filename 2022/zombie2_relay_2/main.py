40bedd4d8a8f344f0912c46c5239dba65734504f
Mark Citron <macitron@mc_tp_sb3.redmond.corp.microsoft.com>
2022-09-13 14:20:06 -0700

    added framework for relay 2

--- 2022/zombie2_relay_2/main.py
+++ 2022/zombie2_relay_2/main.py
@@ -1,15 +1,51 @@
 #!/usr/bin/python3
 
 from flask import Flask, render_template
+from relays import *
 import time
+
+# set up linear actuator relays
+relay1 = LinAct("LinearActuatorOne", 26)
+relay2 = LinAct("LinearActuatorTow", 21)
+relay3 = LinAct("LinearActuatorThree", 20)
 
 # setting up global relay controls
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def relay_mainpage():
     return render_template('index.html')
+
+@app.route('/extend_one/')
+def extend_one():
+    relay1.extend()
+    return render_template("index.html")
+
+@app.route('/contact_one/')
+def contract_one():
+    relay1.contact()
+    return render_template("index.html")
+
+@app.route('/extend_two/')
+def extend_two():
+    relay2.extend()
+    return render_template("index.html")
+
+@app.route('/contract_two/')
+def contract_two():
+    relay2.contract()
+    return render_template("index.html")
+
+@app.route('/extend_three/')
+def extend_three():
+    relay3.extend()
+    return render_template("index.html")
+
+@app.route('/contract_three/')
+def contract_three():
+    relay3.contract()
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
