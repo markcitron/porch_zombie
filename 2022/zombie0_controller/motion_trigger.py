8727498a770f055a69c06576abcbde42486b40a4
Mark Citrion <mark_citron@hotmqail.c>
2022-09-20 00:23:06 -0700

    updating to use configruation file rather than hardcoding the addresses to relay_1 but running into problems with the variable replacement.  Come back later and take a look.

--- 2022/zombie0_controller/motion_trigger.py
+++ 2022/zombie0_controller/motion_trigger.py
@@ -2,12 +2,19 @@
 
 from datetime import datetime
 from datetime import timedelta
+import requests
+import configparser
 
 # import the opencv module
 import cv2
 
 def get_datetime():
     return datetime.now()
+
+def get_relay_one_ip():
+    config = configparser.ConfigParser() 
+    config.read("../hp.cnf")
+    return(config['zombie1_relay_one']['ip'])
 
 def main(): 
     # detection vars
@@ -57,12 +64,18 @@ def main():
         if not movement == prev_movement:
             if movement:
                 # starting motion
-                # TODO: trigger motion in scarecrow
+                start_motion_addy = "http://{}:5000/extend_one/".format(get_relay_one_ip())
+                start_mitoin_addy = "http://10.10.0.14:5000/extend_one/"
+                x = requests.get("http://10.10.0.14:5000/extend_one/")
                 print("Starting motion: {0}".format(current_timestamp))
             else:
                 # stopping motion
-                # TODO: stop motion or do nothing if motion times out on its own.
+                stop_motion_addy = "http://{}:5000/contract_one/".format(get_relay_one_ip())
+                stop_motion_addy = "http://10.10.0.14:5000/contract_one/"
+                x = requests.get("http://10.10.0.14/contract_one/")
                 print("Stopping motion: {0}".format(current_timestamp))
 
 if __name__ == "__main__":
     main()
+#!/usr/bin/python3
+
