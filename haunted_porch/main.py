06fbf6dd62b086af286a35c91461247f46c7a8fa
Mark Citrion <mark_citron@hotmqail.c>
2023-10-22 10:50:18 -0700

    added launcher status

--- haunted_porch/main.py
+++ haunted_porch/main.py
@@ -12,6 +12,18 @@ def hello():
     return render_template('index.html')
     # return render_template('index.html', param1=value1, param2=value2)
 
+@app.route('/launcher_status/')
+def launcher_status():
+    launcher_script = "None"
+    launcher_pid = 0
+    launcher_running = "No"
+    for p in psutil.process_iter():
+        if "launcher" in p.name():
+            launcher_script = p.name();
+            launcher_pid = p.pid
+            launcher_running = "Yes"
+    return render_template('launcher_status.html', script_name=launcher_script, script_id=launcher_pid, script_status=launcher_running)
+
 @app.errorhandler(Exception)
 def basic_error(e):
     return("an error occurred: " + str(e))
