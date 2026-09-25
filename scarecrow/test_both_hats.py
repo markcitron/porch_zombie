7049fc69de01a257c108c531d194f6a0eb12a6d3
Mark Citron <mark_citron@hotmail.com>
2026-09-25 16:36:57 -0700

    creating a test for both hats in same script

---
+++ scarecrow/test_both_hats.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+# load general utils
+from time import sleep
+
+# load library for the 8relay switch hat
+import lib8relind
+from relays import *
+
+# load libraries for the robot hat control
+from robot_hat import Servo, ADC
+from robo_hat.utils import reset_mcu
+
+# Setup the robot hat card
+reset_mcu()
+test_servo = 0 # using servo 0 for testing
+
+# robohat servo test function(s)
+def test_servo():
+    print("Testing servo {}".format(test_servo))
+    which_servo = test_servo
+    servo_start = 90
+    servo_end = -90
+    Servo(int(which_servo)).angle(int(servo_start))
+    sleep(1)
+    Servo(int(which_servo)).angle(int(servo_end))
+    sleep(1)
+    Servo(int(which_servo)).angle(int(servo_start))
+
+    return True
+
+# Setup relays
+relay1 = LinAct("", 1)
+relay2 = LinAct("", 2)
+relay3 = LinAct("", 3)
+relay4 = LinAct("", 4)
+relay5 = LinAct("", 5)
+relay6 = LinAct("", 6)
+relay7 = LinAct("", 7)
+relay8 = LinAct("", 8)
+
+# relay test functions
+def test_all_relays():
+    print("Extending all relays")
+    relay1.extend()
+    relay2.extend()
+    relay3.extend()
+    relay4.extend()
+    relay5.extend()
+    relay6.extend()
+    relay7.extend()
+    relay8.extend()
+
+    sleep(1)
+
+    print("Contracting all relays")
+    relay1.contract()
+    relay2.contract()
+    relay3.contract()
+    relay4.contract()
+    relay5.contract()
+    relay6.contract()
+    relay7.contract()
+    relay8.contract()
+
+    return true
+
+
+def main():
+    print("Testing both hats...")
+
+if __name__ == "__main__":
+    main()
